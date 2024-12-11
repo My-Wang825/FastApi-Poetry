e9bb4b441164228951cda2846ae27966ef28f768
wangmuyang <wangmuyang@wqmail.cn>
2024-12-11 16:08:22 +0800

    first commit

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from api.app.routers import default
+from api.core.config import configs
+import uvicorn
+
+
+
+
+def include_router(app):
+    app.include_router(default.router)
+
+def start_application():
+    app = FastAPI(title=configs.APP_TITLE,version=configs.APP_VERSION)
+    include_router(app)
+    return app
+
+#docker
+# app = start_application()
+
+#local
+if __name__ == "__main__":
+    app = start_application()
+    uvicorn.run(app,host=configs.APP_HOST,port=int(configs.APP_PORT)) 
